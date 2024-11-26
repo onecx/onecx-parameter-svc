f44b33e0da442e1f94b9d259beb46394866decb9
Andrej Petras <andrej@lorislab.org>
2024-11-26 18:23:32 +0100

    feat: update tests

--- src/main/java/org/tkit/onecx/parameters/rs/external/v1/controllers/ParameterRestControllerV1.java
+++ src/main/java/org/tkit/onecx/parameters/rs/external/v1/controllers/ParameterRestControllerV1.java
@@ -7,21 +7,26 @@ import java.util.Map;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
+import jakarta.validation.ConstraintViolationException;
 import jakarta.ws.rs.core.Response;
 
+import org.jboss.resteasy.reactive.RestResponse;
+import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
 import org.tkit.onecx.parameters.domain.daos.ApplicationParameterDAO;
 import org.tkit.onecx.parameters.domain.daos.ApplicationParameterHistoryDAO;
 import org.tkit.onecx.parameters.domain.models.ApplicationParameterHistory;
 import org.tkit.onecx.parameters.rs.external.v1.mappers.ApplicationParameterHistoryMapper;
+import org.tkit.onecx.parameters.rs.external.v1.mappers.ExceptionMapper;
 import org.tkit.quarkus.log.cdi.LogService;
 
-import gen.org.tkit.onecx.parameters.rs.v1.ExternalApi;
+import gen.org.tkit.onecx.parameters.rs.v1.ParameterApi;
 import gen.org.tkit.onecx.parameters.rs.v1.model.ParametersBucketDTOV1;
+import gen.org.tkit.onecx.parameters.rs.v1.model.ProblemDetailResponseDTOV1;
 
 @LogService
 @ApplicationScoped
 @Transactional(value = Transactional.TxType.NOT_SUPPORTED)
-public class ParameterRestControllerV1 implements ExternalApi {
+public class ParameterRestControllerV1 implements ParameterApi {
 
     @Inject
     ApplicationParameterDAO applicationParameterDAO;
@@ -32,6 +37,9 @@ public class ParameterRestControllerV1 implements ExternalApi {
     @Inject
     ApplicationParameterHistoryMapper mapper;
 
+    @Inject
+    ExceptionMapper exceptionMapper;
+
     @Override
     public Response getApplicationParameters(String productName, String appId) {
         Map<String, String> applicationParameters = applicationParameterDAO.findAllByProductNameAndApplicationId(productName,
@@ -41,14 +49,19 @@ public class ParameterRestControllerV1 implements ExternalApi {
 
     @Override
     public Response bucketRequest(String productName, String appId, ParametersBucketDTOV1 dto) {
-        if (dto == null || dto.getParameters().isEmpty()) {
-            return Response.status(Response.Status.OK).build();
+        if (dto.getParameters() == null || dto.getParameters().isEmpty()) {
+            return Response.status(Response.Status.NO_CONTENT).build();
         }
         List<ApplicationParameterHistory> items = new ArrayList<>();
         dto.getParameters().forEach((key, value) -> items
                 .add(mapper.mapItem(value, key, dto, productName, appId,
                         value.getCurrentValue())));
         historyDAO.create(items);
-        return Response.status(Response.Status.OK).build();
+        return Response.status(Response.Status.NO_CONTENT).build();
+    }
+
+    @ServerExceptionMapper
+    public RestResponse<ProblemDetailResponseDTOV1> constraint(ConstraintViolationException ex) {
+        return exceptionMapper.constraint(ex);
     }
 }
