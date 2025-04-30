0894443b2f06f91c451f71476dae03137c13e4fc
JordenReuter <149687553+JordenReuter@users.noreply.github.com>
2025-04-30 09:37:14 +0200

    fix: added string to object mapping (#112)

--- src/main/java/org/tkit/onecx/parameters/rs/bff/v1/mappers/ParameterMapperBffV1.java
+++ src/main/java/org/tkit/onecx/parameters/rs/bff/v1/mappers/ParameterMapperBffV1.java
@@ -4,23 +4,33 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import jakarta.inject.Inject;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.tkit.onecx.parameters.domain.models.Parameter;
+import org.tkit.onecx.parameters.rs.internal.mappers.ParameterMapper;
 import org.tkit.quarkus.rs.mappers.OffsetDateTimeMapper;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import gen.org.tkit.onecx.parameters.rs.v1.bff.model.ParameterBffDTOV1;
 import gen.org.tkit.onecx.parameters.rs.v1.bff.model.ParametersBulkRequestBffDTOV1;
 import gen.org.tkit.onecx.parameters.rs.v1.bff.model.ParametersBulkResponseBffDTOV1;
 
 @Mapper(uses = OffsetDateTimeMapper.class)
-public interface ParameterMapperBffV1 {
+public abstract class ParameterMapperBffV1 {
+    @Inject
+    ObjectMapper objectMapper;
 
     @Mapping(source = "productName", target = "productName")
     @Mapping(source = "applicationId", target = "applicationId")
-    ParameterBffDTOV1 parameterToParameterBffDTOV1(Parameter parameter);
+    @Mapping(target = "value", source = "value", qualifiedByName = "s2o")
+    @Mapping(target = "importValue", source = "importValue", qualifiedByName = "s2o")
+    public abstract ParameterBffDTOV1 parameterToParameterBffDTOV1(Parameter parameter);
 
-    default ParametersBulkResponseBffDTOV1 mapParameters(List<Parameter> parameters, ParametersBulkRequestBffDTOV1 request) {
+    public ParametersBulkResponseBffDTOV1 mapParameters(List<Parameter> parameters, ParametersBulkRequestBffDTOV1 request) {
         ParametersBulkResponseBffDTOV1 bulkResponseBffDTOV1 = new ParametersBulkResponseBffDTOV1();
         Map<String, Map<String, List<ParameterBffDTOV1>>> productsMap = request.getProducts().entrySet().stream()
                 .collect(Collectors.toMap(
@@ -35,5 +45,17 @@ public interface ParameterMapperBffV1 {
                                                 .toList()))));
         bulkResponseBffDTOV1.setProducts(productsMap);
         return bulkResponseBffDTOV1;
+    }
+
+    @Named("s2o")
+    public Object s2o(String value) {
+        if (value == null || value.isBlank()) {
+            return null;
+        }
+        try {
+            return objectMapper.readValue(value, Object.class);
+        } catch (Exception e) {
+            throw new ParameterMapper.MapperException("Error reading parameter value", e);
+        }
     }
 }
