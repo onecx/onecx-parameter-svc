b0f96a2680de2359fe69df8a84674adffa48b2fe
JordenReuter <149687553+JordenReuter@users.noreply.github.com>
2025-07-15 12:21:10 +0200

    fix: hihistory duration calculation (#130)

--- src/main/java/org/tkit/onecx/parameters/domain/timer/MaintenanceHistoryService.java
+++ src/main/java/org/tkit/onecx/parameters/domain/timer/MaintenanceHistoryService.java
@@ -15,7 +15,6 @@ import org.tkit.quarkus.context.Context;
 import org.tkit.quarkus.jpa.exceptions.DAOException;
 import org.tkit.quarkus.jpa.tenant.ContextTenantResolverConfig;
 
-import io.quarkus.runtime.configuration.DurationConverter;
 import io.quarkus.scheduler.Scheduled;
 import lombok.extern.slf4j.Slf4j;
 
@@ -42,9 +41,8 @@ public class MaintenanceHistoryService {
     @Scheduled(identity = "maintenance.history", cron = "${onecx.parameter.scheduler.expression}")
     @Transactional(value = Transactional.TxType.REQUIRES_NEW, rollbackOn = DAOException.class)
     void maintenanceHistoryData() {
-        DurationConverter converter = new DurationConverter();
         LocalDateTime dt = LocalDateTime.now()
-                .minus(converter.convert(parameterConfig.maintenanceHistoryScheduler().duration()));
+                .minusDays(Long.parseLong(parameterConfig.maintenanceHistoryScheduler().duration()));
         try {
             log.info("Scheduler for job id: '{}' started.", JOB_ID);
             var tenantId = Boolean.TRUE.equals(tenantConfig.root().enabled()) ? tenantConfig.root().value()
